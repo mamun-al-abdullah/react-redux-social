0f3dd77792ef92fceac7ba02a49dfa8ce6ec79c2
mamun <mamun@muhammeds-MacBook-Air.local>
2024-10-09 12:17:26 +0600

    init

--- fakebook/src/components/main/middleLayout/Post.tsx
+++ fakebook/src/components/main/middleLayout/Post.tsx
@@ -160,11 +160,11 @@ export default function Post({postId, name, avatar, timeStamp, privacy, status, 
             </div>
             <div className="_feed_inner_timeline_total_reacts _padd_r24 _padd_l24 _mar_b26">
                 <div className="_feed_inner_timeline_total_reacts_image">
-                    <img src="assets/images/react_img1.png" alt="Image" className="_react_img1"/>
-                    <img src="assets/images/react_img2.png" alt="Image" className="_react_img"/>
-                    <img src="assets/images/react_img3.png" alt="Image" className="_react_img _rect_img_mbl_none"/>
-                    <img src="assets/images/react_img4.png" alt="Image" className="_react_img _rect_img_mbl_none"/>
-                    <img src="assets/images/react_img5.png" alt="Image" className="_react_img _rect_img_mbl_none"/>
+                    <img src="assets/images/react_img1.png" alt="Image" className="d-none _react_img1"/>
+                    <img src="assets/images/react_img2.png" alt="Image" className="d-none _react_img"/>
+                    <img src="assets/images/react_img3.png" alt="Image" className="d-none _react_img _rect_img_mbl_none"/>
+                    <img src="assets/images/react_img4.png" alt="Image" className="d-none _react_img _rect_img_mbl_none"/>
+                    <img src="assets/images/react_img5.png" alt="Image" className="d-none _react_img _rect_img_mbl_none"/>
                     <p className="_feed_inner_timeline_total_reacts_para">{likes||0}</p>
                 </div>
                 <div className="_feed_inner_timeline_total_reacts_txt">
@@ -310,7 +310,7 @@ export default function Post({postId, name, avatar, timeStamp, privacy, status, 
                 :
                 null
                 }
-                </div>
+            </div>
         </div>
   )
 }
